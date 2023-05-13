28d0385b2ae364dfe94e1771298117ed4b846dc2
LEAVING-7 <leavingqwq@gmail.com>
2023-05-13 10:27:49 +0800

    add echo server example

--- examples/example_echo.cpp
+++ examples/example_echo.cpp
@@ -7,15 +7,14 @@
 int main()
 {
   auto reactor = async::Reactor();
-  auto executor = async::MutilThreadExecutor(4);
-
+  auto executor = async::InlineExecutor();
   auto listener = async::TcpListener::Bind(reactor, async::SocketAddr {async::SocketAddrV4 {{127, 0, 0, 1}, 8080}});
   if (!listener) {
     std::cout << listener.error().message() << std::endl;
     return 1;
   }
   auto result = executor.block(
-      [](async::MutilThreadExecutor& e, async::Reactor& r, async::TcpListener listener) -> Task<int> {
+      [](async::InlineExecutor& e, async::Reactor& r, async::TcpListener listener) -> Task<int> {
         for (int i = 0; i < 3; i++) {
           auto stream = co_await listener.accept(nullptr);
           if (!stream) {
@@ -23,11 +22,15 @@ int main()
             co_return 1;
           } else {
             std::cout << "new connection" << std::endl;
-            auto buf = "hello world";
+            auto buf = "hi there\n";
             e.spawnDetach(
                 [](async::Reactor& r, char const* msg, async::TcpStream stream) -> Task<> {
+                  auto writableBuf = std::array<uint8_t, 1024> {};
+                  auto readn = co_await stream.read(std::as_writable_bytes(std::span(writableBuf)));
+                  assert(readn);
                   auto buf = std::string_view(msg);
-                  auto n = co_await stream.write(std::as_bytes(std::span(buf)));
+                  auto writen = co_await stream.write(std::as_bytes(std::span(writableBuf)));
+                  assert(writen);
                   co_return;
                 }(r, buf, std::move(stream.value())),
                 r);

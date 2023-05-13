a8add421e1587ede2330a9deea9c02360687358d
LEAVING-7 <leavingqwq@gmail.com>
2023-05-13 10:49:35 +0800

    add http server examples

---
+++ examples/example_http_hello.cpp
@@ -0,0 +1,41 @@
+#include <Async/Executor.hpp>
+#include <Async/Reactor.hpp>
+#include <Async/TcpListener.hpp>
+
+static auto gExecutor = async::MutilThreadExecutor(8);
+static auto gReactor = async::Reactor();
+
+int main()
+{
+  auto listener = async::TcpListener::Bind(gReactor, async::SocketAddr {async::SocketAddrV4 {{127, 0, 0, 1}, 8080}});
+  if (!listener) {
+    std::cout << listener.error().message() << std::endl;
+    return 1;
+  } else {
+    gExecutor.block(
+        [](async::TcpListener listener) -> Task<> {
+          for (int i = 0; i < 1000; i++) {
+            auto stream = co_await listener.accept(nullptr);
+            if (!stream) {
+              std::cout << stream.error().message() << std::endl;
+              co_return;
+            } else {
+              auto buf = "HTTP/1.1 200 OK\r\nContent-Length: 12\r\n\r\nHello World\n";
+              gExecutor.spawnDetach(
+                  [](char const* msg, async::TcpStream stream) -> Task<> {
+                    auto writableBuf = std::array<uint8_t, 1024> {};
+                    auto readn = co_await stream.read(std::as_writable_bytes(std::span(writableBuf)));
+                    assert(readn);
+                    auto buf = std::string_view(msg);
+                    auto writen = co_await stream.write(std::as_bytes(std::span(buf)));
+                    assert(writen);
+                    co_return;
+                  }(buf, std::move(stream.value())),
+                  gReactor);
+            }
+          }
+          co_return;
+        }(std::move(listener.value())),
+        gReactor);
+  }
+}
